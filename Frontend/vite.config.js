965a4a31333de6cf073d573e4004ca080df12acd
louis-129 <128095517+louis-129@users.noreply.github.com>
2025-01-24 23:46:10 -0800

    Update vite.config.js
    Adding Tailwind config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import vitePluginRequire from "vite-plugin-require";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginRequire.default()],
+  plugins: [react(), vitePluginRequire.default(),
+    tailwindcss(),
+  ],
 })
+
+
